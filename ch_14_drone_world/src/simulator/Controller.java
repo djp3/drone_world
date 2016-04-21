1e62c5304a57810f05521051bab157d9e4f0e513
Don Patterson <d_j_p_3@djp3.net>
2016-04-21 14:59:16 -0700

    Adding javadoc for new functions to interface

--- ch_14_drone_world/src/simulator/Controller.java
+++ ch_14_drone_world/src/simulator/Controller.java
@@ -2,6 +2,18 @@ package simulator;
 
 public interface Controller {
 
+	/**
+	 * return true if you want the drone's fully rendered, which takes a little more time on start 
+	 */
+	boolean isHighResolution();
+	/**
+	 * How fast do you want the simulator to run?
+	 * 1 is real-time (1 simulator second is one real-second)
+	 * 100 is faster (as fast as possible);
+	 * 
+	 */
+	long simulatorSpeed();
+	
 	void setSimulator(Simulator simulator);
 	
 	void droneEmbarkingStart(Drone drone);
