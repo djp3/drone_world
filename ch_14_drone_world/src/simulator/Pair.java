f92fd28e691ab17e40ddaf702834158afb973c85
Don Patterson <d_j_p_3@djp3.net>
2016-04-28 02:57:16 -0700

    Implement a Pair class for convenience.

---
+++ ch_14_drone_world/src/simulator/Pair.java
@@ -0,0 +1,31 @@
+package simulator;
+
+public class Pair<T1, T2> {
+	
+	private T1 key;
+	private T2 value;
+
+	public Pair(T1 k,T2 v){
+		key = k;
+		value = v;
+	}
+
+	public T1 getKey() {
+		return key;
+	}
+
+	public void setKey(T1 key) {
+		this.key = key;
+	}
+
+	public T2 getValue() {
+		return value;
+	}
+
+	public void setValue(T2 value) {
+		this.value = value;
+	}
+	
+	
+
+}
